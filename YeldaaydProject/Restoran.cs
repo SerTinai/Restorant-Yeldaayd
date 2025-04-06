bd7d340e709ca7cc26cee84bafc4adb913bfd695
ServerRoot <118677976+SerTinai@users.noreply.github.com>
2025-04-06 09:29:18 +0300

    Add files via upload

---
+++ YeldaaydProject/Restoran.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YeldaaydProject
+{	public class Restoran
+	{
+		public string restoranName;
+		public int chairNumber;
+		public Restoran() 
+		{
+			chairNumber = 16;
+		}
+		public string MessagePrint()
+		{
+			return $"it is Restoran Name: {restoranName} and chair number: {chairNumber} ";
+		}
+	}
+}
